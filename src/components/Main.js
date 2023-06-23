397c1ecc41ff0bdcfda2dfab0db3a6853656f03b
a-williams8427 <majorxace@hotmail.com>
2023-06-22 23:47:56 -0400

    Preparing for next parts of the homepage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Specials from "./Specials";
+import Testimonials from "./Testimonials";
 
 function Main() {
-  return (
-    <main>Main</main>
-  )
+    return (
+        <main style={{ marginLeft: "25px", marginRight: "25px" }}>
+            <Specials />
+            <Testimonials />
+        </main>
+    );
 }
 
-export default Main
+export default Main;
--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Box, Grid, Typography } from "@mui/material";
+import React from "react";
 
 function Specials() {
-  return (
-    <div>Specials</div>
-  )
+    return (
+        <div>
+            <Box
+                sx={{
+                    display: "block",
+                    height: "10vw",
+                }}
+            />
+            <Grid container>
+                <Grid item xs={12}>
+                    <Typography variant="h3" color="text">
+                        Specials
+                    </Typography>
+                </Grid>
+                {Array.from(Array(12)).map((_, index) => (
+                    <Grid item xs={12} md={4} lg={3} key={index}>
+                        Special here
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
 }
 
-export default Specials
+export default Specials;
