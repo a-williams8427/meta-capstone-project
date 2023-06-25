ef1390fb5b7322b144c94607f51536508cd0ad14
a-williams8427 <majorxace@hotmail.com>
2023-06-24 22:06:59 -0400

    Updated validations used the form wrapper

--- src/pages/BookingPage/BookingForm.js
+++ src/pages/BookingPage/BookingForm.js
@@ -1,22 +1,13 @@
 import React from "react";
 import dayjs from "dayjs";
-import {
-    Box,
-    FormControl,
-    Grid,
-    InputAdornment,
-    InputLabel,
-    MenuItem,
-    Select,
-    Stack,
-    TextField,
-} from "@mui/material";
+import { InputAdornment, MenuItem, Stack, TextField } from "@mui/material";
 import GroupIcon from "@mui/icons-material/Group";
 import { DatePicker, TimePicker } from "@mui/x-date-pickers";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import YellowButton from "../../components/YellowButton";
+import FormWrapper from "./FormWrapper";
 
 const openHour = dayjs().set("hour", 17).startOf("hour");
 const closeHour = dayjs().set("hour", 22).startOf("hour");
@@ -27,22 +18,7 @@ const maxDiners = 16;
 const occasions = ["Party", "Business", "Holiday", "Gathering"];
 
 const schema = yup.object().shape({
-    bookDate: yup
-        .mixed()
-        .required("Please select a date")
-        .test(
-            "futureDate",
-            "Please choose a date that hasn't passed",
-            function (value) {
-                const currentDate = dayjs().startOf("day");
-                const selectedDate = dayjs(value).startOf("day");
-
-                return (
-                    selectedDate.isSame(currentDate) ||
-                    selectedDate.isAfter(currentDate)
-                );
-            }
-        ),
+    bookDate: yup.mixed().required("Please select a date"),
     bookTime: yup
         .mixed()
         .required("Please select a time")
@@ -72,10 +48,23 @@ const schema = yup.object().shape({
                         path: "bookTime",
                     });
                 }
+                if (
+                    selectedDateTime
+                        .startOf("day")
+                        .isSame(currentDate.startOf("day")) &&
+                    selectedDateTime.isBefore(currentDate)
+                ) {
+                    return this.createError({
+                        message: `Please select a time after ${currentDate.format(
+                            "hh:mm A"
+                        )}`,
+                        path: "bookTime",
+                    });
+                }
                 if (selectedDateTime.isBefore(currentDate)) {
                     return this.createError({
-                        message: "Please select a date and time in the future",
-                        path: "bookTime",
+                        message: "Please select a date in the future.",
+                        path: "bookDate",
                     });
                 }
 
@@ -91,7 +80,7 @@ const schema = yup.object().shape({
     seating: yup.string().required("Please select a seating option"),
 });
 
-function BookingForm() {
+function BookingForm({ handleStep }) {
     const {
         register,
         handleSubmit,
@@ -112,6 +101,7 @@ function BookingForm() {
 
     const onSubmit = (data) => {
         console.log(data);
+        handleStep();
     };
 
     //console.log(errors.diners);
@@ -119,137 +109,118 @@ function BookingForm() {
 
     return (
         <>
-            <Box
-                component="form"
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit(onSubmit)}
-                paddingTop={"2rem"}
-            >
+            <FormWrapper onSubmit={handleSubmit(onSubmit)}>
                 <Stack
-                    direction="column"
+                    direction="row"
                     justifyContent="center"
                     alignItems="center"
                     spacing={{ xs: 1, sm: 2 }}
-                    maxWidth={"450px"}
-                    width={"100%"}
-                    marginLeft={"auto"}
-                    marginRight={"auto"}
                 >
-                    <Stack
-                        direction="row"
-                        justifyContent="center"
-                        alignItems="center"
-                        spacing={{ xs: 1, sm: 2 }}
-                    >
-                        <Controller
-                            control={control}
-                            name="bookDate"
-                            render={({ field }) => (
-                                <DatePicker
-                                    {...field}
-                                    label="Date"
-                                    disablePast
-                                    onChange={(e) => field.onChange(e)}
-                                    onClose={() => trigger(field.name)}
-                                    value={field.value || null}
-                                    slotProps={{
-                                        textField: {
-                                            error: !!errors.bookDate,
-                                            helperText:
-                                                errors.bookDate?.message,
-                                            fullWidth: true,
-                                        },
-                                    }}
-                                />
-                            )}
-                        />
-                        <Controller
-                            control={control}
-                            name="bookTime"
-                            render={({ field }) => (
-                                <TimePicker
-                                    {...field}
-                                    label="Time"
-                                    minTime={openHour}
-                                    maxTime={closeHour}
-                                    closeOnSelect={false}
-                                    onChange={(e) => field.onChange(e)}
-                                    onClose={() => trigger(field.name)}
-                                    value={field.value || null}
-                                    slotProps={{
-                                        textField: {
-                                            error: !!errors.bookTime,
-                                            helperText:
-                                                errors.bookTime?.message,
-                                            fullWidth: true,
-                                        },
-                                    }}
-                                />
-                            )}
-                        />
-                    </Stack>
-                    <TextField
-                        label="Number of Diners"
-                        type="number"
-                        fullWidth
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <GroupIcon />
-                                </InputAdornment>
-                            ),
-                        }}
-                        inputProps={{
-                            min: minDiners,
-                            max: maxDiners,
-                        }}
-                        {...register("diners")}
-                        error={!!errors.diners}
-                        helperText={errors.diners?.message}
-                    />
                     <Controller
-                        name="occasion"
                         control={control}
+                        name="bookDate"
                         render={({ field }) => (
-                            <TextField
+                            <DatePicker
                                 {...field}
-                                label="Occasion"
-                                select
-                                fullWidth
-                                error={!!errors.occasion}
-                                helperText={errors.occasion?.message}
-                            >
-                                {occasions.map((occasion) => (
-                                    <MenuItem key={occasion} value={occasion}>
-                                        {occasion}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
+                                label="Date"
+                                disablePast
+                                onChange={(e) => field.onChange(e)}
+                                onClose={() => trigger(field.name)}
+                                value={field.value || null}
+                                slotProps={{
+                                    textField: {
+                                        error: !!errors.bookDate,
+                                        helperText: errors.bookDate?.message,
+                                        fullWidth: true,
+                                    },
+                                }}
+                            />
                         )}
                     />
                     <Controller
-                        name="seating"
                         control={control}
+                        name="bookTime"
                         render={({ field }) => (
-                            <TextField
+                            <TimePicker
                                 {...field}
-                                label="Seating Option"
-                                select
-                                fullWidth
-                                error={!!errors.seating}
-                                helperText={errors.seating?.message}
-                            >
-                                <MenuItem value={"Indoor"}>Indoor</MenuItem>
-                                <MenuItem value={"Outdoor"}>Outdoor</MenuItem>
-                            </TextField>
+                                label="Time"
+                                minTime={openHour}
+                                maxTime={closeHour}
+                                closeOnSelect={false}
+                                onChange={(e) => field.onChange(e)}
+                                onClose={() => trigger(field.name)}
+                                value={field.value || null}
+                                slotProps={{
+                                    textField: {
+                                        error: !!errors.bookTime,
+                                        helperText: errors.bookTime?.message,
+                                        fullWidth: true,
+                                    },
+                                }}
+                            />
                         )}
                     />
-                    <YellowButton type="submit" variant="contained" fullWidth>
-                        Let's Go
-                    </YellowButton>
                 </Stack>
-            </Box>
+                <TextField
+                    label="Number of Diners"
+                    type="number"
+                    fullWidth
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <GroupIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    inputProps={{
+                        min: minDiners,
+                        max: maxDiners,
+                    }}
+                    {...register("diners")}
+                    error={!!errors.diners}
+                    helperText={errors.diners?.message}
+                />
+                <Controller
+                    name="occasion"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            label="Occasion"
+                            select
+                            fullWidth
+                            error={!!errors.occasion}
+                            helperText={errors.occasion?.message}
+                        >
+                            {occasions.map((occasion) => (
+                                <MenuItem key={occasion} value={occasion}>
+                                    {occasion}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    )}
+                />
+                <Controller
+                    name="seating"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            label="Seating Option"
+                            select
+                            fullWidth
+                            error={!!errors.seating}
+                            helperText={errors.seating?.message}
+                        >
+                            <MenuItem value={"Indoor"}>Indoor</MenuItem>
+                            <MenuItem value={"Outdoor"}>Outdoor</MenuItem>
+                        </TextField>
+                    )}
+                />
+                <YellowButton type="submit" variant="contained" fullWidth>
+                    Continue
+                </YellowButton>
+            </FormWrapper>
         </>
     );
 }
