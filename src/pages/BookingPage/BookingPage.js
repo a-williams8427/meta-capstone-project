df7e23d48d1d27bc94c5c85856dcba4ece0616e2
a-williams8427 <majorxace@hotmail.com>
2023-06-25 03:12:40 -0400

    Switched to using context for booking

--- src/pages/BookingPage/BookingPage.js
+++ src/pages/BookingPage/BookingPage.js
@@ -2,55 +2,70 @@ import React, { useState } from "react";
 import BookingForm from "./BookingForm";
 import Chicago from "../../components/Chicago";
 import { Box, Typography } from "@mui/material";
+import dayjs from "dayjs";
+import { BookingContext } from "../../Contexts/BookingContext";
 import PersonalInfoForm from "./PersonalInfoForm";
 
 function BookingPage() {
     const [currentStep, setCurrentStep] = useState(0);
 
+    const [formData, setFormData] = useState({});
+
+    const handleFormData = (data) => {
+        setFormData((prevData) => ({ ...prevData, ...data }));
+    };
+
     const handleStep = () => {
         setCurrentStep((prevStep) => prevStep + 1);
     };
 
-    //Placeholder for when I use the form data
-    //`${seating}table for ${diners} diners on ${bookDate} at ${bookTime} `
     const subTitles = [
         "Find a table for any occasion",
-        "Who is the reservation for?",
+        `${formData.seating} table for ${formData.diners} diners on ${dayjs(
+            formData.bookDate
+        ).format("MM/DD/YYYY")} at ${dayjs(formData.bookTime).format(
+            "hh:mm A"
+        )}`,
     ];
 
+    console.log(formData);
     return (
         <>
-            <Box
-                sx={{
-                    width: "100%",
-                    backgroundColor: "primary.main",
-                }}
+            <BookingContext.Provider
+                value={{ handleFormData, handleStep, formData }}
             >
+                <Box
+                    sx={{
+                        width: "100%",
+                        backgroundColor: "primary.main",
+                    }}
+                >
+                    <div style={{ marginLeft: "25px", marginRight: "25px" }}>
+                        <Chicago />
+                        <Typography
+                            variant="h6"
+                            color="primary.contrastText"
+                            component="h3"
+                            paddingTop="5rem"
+                            paddingBottom="1rem"
+                        >
+                            {subTitles[currentStep]}
+                        </Typography>
+                    </div>
+                </Box>
                 <div style={{ marginLeft: "25px", marginRight: "25px" }}>
-                    <Chicago />
-                    <Typography
-                        variant="h6"
-                        color="primary.contrastText"
-                        component="h3"
-                        paddingTop="5rem"
-                        paddingBottom="1rem"
-                    >
-                        {subTitles[currentStep]}
-                    </Typography>
+                    {currentStep === 0 && (
+                        <>
+                            <BookingForm />
+                        </>
+                    )}
+                    {currentStep === 1 && (
+                        <>
+                            <PersonalInfoForm />
+                        </>
+                    )}
                 </div>
-            </Box>
-            <div style={{ marginLeft: "25px", marginRight: "25px" }}>
-                {currentStep === 0 && (
-                    <>
-                        <BookingForm handleStep={handleStep} />
-                    </>
-                )}
-                {currentStep === 1 && (
-                    <>
-                        <PersonalInfoForm />
-                    </>
-                )}
-            </div>
+            </BookingContext.Provider>
         </>
     );
 }
