efd81f953d1a1ccd1b1c6b25e8cc0a7a4edb2054
MajorXAcE <majorxace@hotmail.com>
2023-06-29 20:16:20 -0400

    Completed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import "./Footer.css";
+import logo from "../assets/icons/logo small.png";
 
 function Footer() {
-    //TODO: Finish and add where necessary
-    return <footer>Footer</footer>;
+    return (
+        <footer>
+            <div>
+                <Stack
+                    direction={"row"}
+                    alignItems="center"
+                    justifyContent="space-around"
+                    spacing={2}
+                >
+                    <img src={logo} alt="Little Lemon Logo" />
+                    <Typography>&copy; Copyright Little Lemon</Typography>
+                </Stack>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
